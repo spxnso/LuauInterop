047a80ffc07fd26d7569fb6d71c4eb4e2ccd0d1a
Evann Borde <146754550+spxnso@users.noreply.github.com>
2026-05-26 07:03:10 +0200

    initial commit

---
+++ src/Luau.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using Luau.Native;
+
+class Program {
+    static void Main(string[] args) {
+        LuaState L = new(NativeMethods.luaL_newstate());
+
+        try {
+            L.OpenLibraries();
+
+            // Awesome stuff here.
+        } catch (Exception ex) {
+            Console.WriteLine($"Error: {ex.Message}");
+        } finally {
+            L.Close();
+        }
+    }
+};
